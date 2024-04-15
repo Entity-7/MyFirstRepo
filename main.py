fdaac5f846d3fc7ff675df2bc8a05221b978a69d
Entity-7 <ahmedehab.ie@gmail.com>
2024-04-15 20:07:37 +0200

    Updated Main main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def add(number1, number2):
     return number1 + number2
 
+def subtract(number1, number2):
+    return number1 - number2
 
 if __name__ == '__main__':
-    result = add(5, 5)
-    print(result)
+    result1 = add(5, 5)
+    result2 = subtract(5, 3)
+    print(result1, result2)
