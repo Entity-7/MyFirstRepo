17b82a971c755efe1e6d2e50b097e3b53f1ce653
Entity-7 <ahmedehab.ie@gmail.com>
2024-04-15 20:24:54 +0200

    Added a branch function main.py

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ def add(number1, number2):
 def subtract(number1, number2):
     return number1 - number2
 
+def isThisBranch():
+    print("This is a branch")
+
 if __name__ == '__main__':
     result1 = add(5, 5)
     result2 = subtract(5, 3)
     print(result1, result2)
+    isThisBranch()
